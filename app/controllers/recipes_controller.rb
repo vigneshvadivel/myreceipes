75a50f4a02e2f6e5c02f674a1cb1132d5e1e6a8d
vignesh <vigneshvelit@gmail.com>
2016-05-17 07:38:19 +0000

    create new and edit form partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,48 @@
+class RecipesController < ApplicationController
+
+def index
+  @recipes = Recipe.all
+end
+def show
+    @recipe = Recipe.find(params[:id])
+end
+def new
+  @recipe = Recipe.new
+end 
+def create 
+  @recipe = Recipe.new(recipe_params)
+  @recipe.chef = Chef.find(2)  #after parivate create this type
+  
+  if @recipe.save
+    flash[:success] = "your recipe was cretaed successfully"
+    redirect_to recipes_path
+  else
+    render :new
+  end
+end
+
+def edit
+  @recipe = Recipe.find(params[:id])
+end
+
+def update
+  @recipe = Recipe.find(params[:id])
+  if @recipe.update(recipe_params)
+    flash[:success] = "your recipe was updated successfully"
+    redirect_to recipes_path(@recipe)
+    
+  else
+    render :edit
+end
+end
+
+
+
+
+
+private 
+def recipe_params
+  params.require(:recipe).permit(:name, :summery, :description)
+end
+end
+
